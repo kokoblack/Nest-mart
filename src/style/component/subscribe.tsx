749713e2628ec4150c41c71c3a71c98b16aa3056
kokoblack <benzimakareem26@gmail.com>
2024-02-28 22:26:34 +0100

    added responsiveness

--- src/style/component/subscribe.tsx
+++ src/style/component/subscribe.tsx
@@ -1,23 +1,32 @@
 import { css } from "../../../styled-system/css";
 
 export const container = css.raw({
-    w: '25rem',
+    w: ["16rem", "16rem", "16rem", "25rem"],
     bg: 'secondary.500',
     rounded: '5rem',
     boxSizing: 'border-box',
+
+    "@media screen and (max-width: 479px)": {
+        columnGap: ".5rem"
+      },
 })
 
 export const icon = css({
     color: 'secondary.600',
     ml: '1rem',
-    fontSize: '1.5rem'
+    fontSize: '1.5rem',
+
+    '@media screen and (max-width: 767px)': {
+        fontSize: '1.1rem'
+    },
 })
 
 export const input = css({
     fontFamily: 'quicksand',
-    fontSize: '1rem',
+    fontSize: [".8rem", ".8rem", ".8rem", "1rem"],
     bg: 'transparent',
     mr: 'auto',
+    w: ['6rem'],
 
     _focus: {
         outline: 'none',
@@ -26,5 +35,13 @@ export const input = css({
 
     _placeholder: {
         color: "secondary.600"
-    }
+    },
+})
+
+export const subscribeButton = css.raw({
+    '@media screen and (max-width: 767px)': {
+        px: "1.2rem",
+        py: ".6rem",
+        fontSize: '.8rem'
+    },
 })
