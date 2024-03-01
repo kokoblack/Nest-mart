0296e885ff8d8f94b8984b007d875276e8fa505c
kokoblack <benzimakareem26@gmail.com>
2024-03-01 20:07:48 +0100

    added animation

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -5,17 +5,19 @@ import {
   HeroActive,
   HeroBigText,
   HeroContainer,
+  HeroDiscountContainer,
   HeroIndicator,
   HeroIndicatorCont,
   HeroNavigation,
   HeroSmallText,
 } from "../../style/home/hero";
 import { flex } from "../../style/recipe/flex";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const Hero = () => {
   const [active, setActive] = useState(0);
 
+  const animate = useRef<HTMLDivElement>(null!);
   const text = [
     {
       text: "Fresh Vegetable",
@@ -30,25 +32,33 @@ const Hero = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       if (active === 0) {
-        setActive(1)
-      }else {
-        setActive(0)
+        setActive(1);
+        animate.current.style.animation = "slideOut .5s ease-in alternate";
+      } else {
+        setActive(0);
+        animate.current.style.animation = "slideIn .5s ease-in alternate";
       }
-    }, 5000)
+    }, 5000);
 
-    return () => clearInterval(interval)
+    return () => clearInterval(interval);
   }, [active]);
 
   return (
-    <section className={css(HeroContainer, { bgImage: text[active].url })}>
-      <h3 className={css(HeroBigText)}>
-        {text[active].text} <br /> Big discount
-      </h3>
-      <p className={css(HeroSmallText)}>
-        Save up to 50% off on your first order
-      </p>
+    <section className={css(HeroContainer)}>
+      <div
+        ref={animate}
+        onAnimationEnd={() => (animate.current.style.animation = "none")}
+        className={css({ bgImage: text[active]?.url }, HeroDiscountContainer)}
+      >
+        <h3 className={css(HeroBigText)}>
+          {text[active]?.text} <br /> Big discount
+        </h3>
+        <p className={css(HeroSmallText)}>
+          Save up to 50% off on your first order
+        </p>
 
-      <Subscribe />
+        <Subscribe />
+      </div>
 
       <span
         onClick={() => setActive(0)}
@@ -64,7 +74,7 @@ const Hero = () => {
           right: [".5rem", ".5rem", ".5rem", ".5rem", "1rem"],
         })}
       >
-        <MdKeyboardArrowRight />
+        <MdKeyboardArrowRight className="bounce" />
       </span>
 
       <div className={css(HeroIndicatorCont, flex.raw({ columnGap: "xsm" }))}>
--- src/style/home/hero.tsx
+++ src/style/home/hero.tsx
@@ -1,17 +1,23 @@
-import { css } from "../../../styled-system/css";
+import { css, } from "../../../styled-system/css";
 
 export const HeroContainer = css.raw({
+  w: "100%",
+  my: ["1rem", "1rem", "1rem", "2rem"],
+  rounded: "1.2rem",
+  boxSizing: "border-box",
+  position: "relative",
+  overflow: 'hidden'
+});
+
+export const HeroDiscountContainer = css.raw({
   bgPosition: "center",
   bgSize: "cover",
   w: "100%",
-  my: ["1rem", "1rem", "1rem", "2rem"],
-  rounded: "1rem",
   pl: ["1rem", "1rem", "2rem", "3rem", "5rem"],
   pt: ["1rem", "1rem", "2rem", "4rem"],
-  pb: "1rem",
-  boxSizing: "border-box",
-  position: "relative",
-});
+  pb: ['3rem', '3rem', '3rem', '6rem'],
+  rounded: "1.2rem",
+})
 
 export const HeroBigText = css.raw({
   fontSize: ["1.9rem", "1.9rem", "2.8rem", "4rem"],
@@ -24,7 +30,7 @@ export const HeroBigText = css.raw({
 export const HeroSmallText = css.raw({
   fontSize: [".7rem", ".7rem", "1.1rem", "1.5rem"],
   color: "secondary.400",
-  mb: [".7rem", ".7rem", "1rem", "2rem"],
+  mb: [".9rem", ".9rem", "1rem", "2rem"],
   fontWeight: "500",
 });
 
@@ -38,6 +44,9 @@ export const HeroNavigation = css.raw({
   zIndex: "3",
   top: "50%",
   translateY: "50%",
+  cursor: 'pointer',
+  transition: 'all 2s ease-in ',
+  animation: 'bounce 2s ease-in infinite alternate',
 
   "@media screen and (max-width: 767px)": {
     display: "none",
@@ -45,8 +54,10 @@ export const HeroNavigation = css.raw({
 });
 
 export const HeroIndicatorCont = css.raw({
-  mt: ["1rem", "1rem", "2rem", "3rem"],
-  mr: ["1rem", "1rem", "2rem", "3rem", "5rem"],
+  position: 'absolute',
+  top: ['90%', '90%', '95%'],
+  left: '50%',
+  translateX: '50%'
 });
 
 export const HeroIndicator = css.raw({
