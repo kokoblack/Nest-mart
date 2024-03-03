d0f6115e9afab88d9ad0e27982d75b707b23a6d3
kokoblack <benzimakareem26@gmail.com>
2024-03-03 05:07:21 +0100

    implement the style

--- src/style/component/home/productCard.tsx
+++ src/style/component/home/productCard.tsx
@@ -7,11 +7,25 @@ export const productCardContainer = css.raw({
     bg: 'secondary.500',
     border: '1px solid #ECECEC',
     fontSize: '.8rem',
-    color: 'secondary.700'
+    color: 'secondary.700',
+    position: 'relative'
 })
 
 export const productCardImg = css.raw({
-    w: ['13rem'],
-    h: ['13rem'],
+    w: ['10rem', '10rem','13rem'],
+    h: ['10rem', '10rem', '13rem'],
     mx: 'auto'
+})
+
+export const productCardSpecial = css.raw({
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    borderTopLeftRadius: '.9rem',
+    borderBottomRightRadius: '.9rem',
+    fontSize: '.6rem',
+    px: '1rem',
+    py: '.4rem',
+    color: 'secondary.500',
+    fontFamily: 'lato'
 })
---
+++ src/type/types.tsx
@@ -0,0 +1,11 @@
+export type ProductCardProps = {
+  name: string;
+  category: string;
+  rating: string;
+  brand: string;
+  currentPrice: string;
+  initialPrice: string;
+  color: string;
+  type: string;
+  image: string;
+};
