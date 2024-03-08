39b34f1228e273998bd0cbc4562a3cf716e0219d
kokoblack <benzimakareem26@gmail.com>
2024-03-08 20:53:28 +0100

    implemented rsponsiveness

--- src/pages/home/FeaturedCategories.tsx
+++ src/pages/home/FeaturedCategories.tsx
@@ -2,8 +2,22 @@ import { TiArrowLeft, TiArrowRight } from "react-icons/ti";
 import FeaturedSmallCards from "../../components/home/FeaturedSmallCards";
 import FeaturedBigCard from "../../components/home/FeaturedBigCard";
 import { data } from "../../data/featuredSmallCard";
+import { flex } from "../../style/recipe/flex";
+import { css } from "../../../styled-system/css";
+import {
+  featureUlCont,
+  featuredContainer,
+  featuredFirstSection,
+  featuredIcon,
+  featuredIconCont,
+  featuredSecondSection,
+  featuredThirdSection,
+} from "../../style/home/feaured";
+import { useState } from "react";
 
 const FeaturedCategories = () => {
+  const [active, setActive] = useState(0);
+
   const link = ["Cake & Milk", "Coffes & Teas", "Pet Foods", "Vegetables"];
   const title = [
     "Everyday Fresh & Clean with Our Products",
@@ -11,39 +25,60 @@ const FeaturedCategories = () => {
     "The best Organic Products Online",
   ];
   return (
-    <section>
-      <section>
-        <h3>Featured Categories</h3>
+    <section className={css(featuredContainer)}>
+      <section
+        className={css(
+          flex.raw({ columnGap: "xlg", type: "endY" }),
+          featuredFirstSection
+        )}
+      >
+        <h3 className={css({ fontWeight: "bold", lineHeight: "2rem" })}>
+          Featured Categories
+        </h3>
         <nav>
-          <ul>
+          <ul className={css(flex.raw({ columnGap: "md" }), featureUlCont)}>
             {link.map((data, index) => (
-              <li key={index}>{data}</li>
+              <li
+                onClick={() => setActive(index)}
+                key={index}
+                className={css({
+                  color: active === index ? "primary.100" : "null",
+                  cursor: "pointer",
+                  flexShrink: "0",
+                })}
+              >
+                {data}
+              </li>
             ))}
           </ul>
         </nav>
-        <div>
-          <span>
+        <div className={css(flex.raw({ columnGap: "sm" }), featuredIconCont)}>
+          <span className={css(featuredIcon)}>
             <TiArrowLeft />
           </span>
-          <span>
-            <TiArrowRight/>
+          <span className={css(featuredIcon)}>
+            <TiArrowRight />
           </span>
         </div>
       </section>
 
-      <section>
-        <FeaturedSmallCards
-          img={data[0].img}
-          name={data[0].name}
-          qty={data[0].qty}
-          bg={data[0].bg}
-        />
+      <section
+        className={css(
+          flex.raw({ columnGap: "md", wrap: "no" }),
+          featuredSecondSection
+        )}
+      >
+        {data.map((item, index) => (
+          <FeaturedSmallCards key={index} {...item} />
+        ))}
       </section>
 
-      <section>
-        <FeaturedBigCard title={title[0]} num={1}/>
-        <FeaturedBigCard title={title[1]} num={2}/>
-        <FeaturedBigCard title={title[2]} num={3}/>
+      <section
+        className={css(flex.raw({ columnGap: "md" }), featuredThirdSection)}
+      >
+        <FeaturedBigCard title={title[0]} num={1} />
+        <FeaturedBigCard title={title[1]} num={2} />
+        <FeaturedBigCard title={title[2]} num={3} />
       </section>
     </section>
   );
