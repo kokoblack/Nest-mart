b371a4381945fb1d07803fcb5eaf2e08e98662f2
Haruna Abdul Kareem <benzimakareem26@gmail.com>
2025-01-25 12:58:01 +0100

    added product comp

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,11 +1,13 @@
 import Banner2 from "../../components/global/Banner2";
 import Category from "../../components/layouts/Category";
+import Product from "../../components/layouts/product";
 
 const Blog = () => {
   return (
     <main>
       <Banner2 text="Blog" />
       <Category/>
+      <Product text="Trending Now"/>
     </main>
   );
 };
