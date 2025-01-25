f698749d19120ea2f376468f35010ecee479f0f3
Haruna Abdul Kareem <benzimakareem26@gmail.com>
2025-01-25 13:45:47 +0100

    added gallery comp

--- src/components/layouts/Product.tsx
+++ src/components/layouts/Product.tsx
@@ -1,12 +1,12 @@
 import Headline from "../global/Headline";
 import { Article3, Article4, Article5, Article6 } from ".";
 import { css } from "../../../styled-system/css";
-import { productCont } from "../../style/component/layouts/product";
 import ProductsCard from "./ProductsCard";
+import { SidebarCardCont } from "../../style/global";
 
 const Product = ({ text }: { text: string }) => {
   return (
-    <section className={css(productCont)}>
+    <section className={css(SidebarCardCont)}>
       <Headline title={text} />
 
       <ProductsCard img={Article3} price="$99.50" name="Chen Cardigan" />
